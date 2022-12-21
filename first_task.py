32edb92026410369a05464f087b589528090797b
blinmakersha <valentinakulg@gmail.com>
2022-12-21 17:01:49 +0300

    Added the first task of the homework.

--- first_task.py
+++ first_task.py
@@ -1,8 +1,20 @@
+# с помощью данной функции, мы находим оптимальные по цене билеты в города А и Б.
+# сложность: O(n log n).
 
 def twoCitySchedCost(costs):
+    """
+    Returns:
+        res: the sum of prices for the tickets
+    """
+
+    # сортируем списки в списке costs, через разницу первого и второго элемента.
     costs.sort(key=lambda x: x[0]-x[1])
+
+    # делим нацело длину списка costs.
     half = len(costs) // 2
     res = 0
+
+    # через цикл добавляем самые оптимальные по цене билеты.
     for el in costs:
         half -= 1
         if half < 0:
